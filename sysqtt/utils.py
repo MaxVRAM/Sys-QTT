dfac232d66789ea5fb4e0abfba0a2a457f744d8c
Chris <maxvram@chrisvik.com>
2021-11-01 16:58:54 +1100

    Added protection against non-ASCII characters in cat outputs 	modified:   sysqtt/utils.py

--- sysqtt/utils.py
+++ sysqtt/utils.py
@@ -3,13 +3,13 @@ import sh
 
 def quick_cat(arg):
     output = subprocess.run(['cat', arg], capture_output=True)
-    return output.stdout.decode('utf-8').rstrip() if output.returncode == 0 else None
+    return output.stdout.decode('utf-8', errors='strict').strip() if output.returncode == 0 else None
 
 def command_find(command, term):
     value = 'Unknown'
     try:
         proc = subprocess.run(command, capture_output=True)
-        output = proc.stdout.decode('utf-8').rstrip()
+        output = proc.stdout.decode('utf-8', errors='strict').strip()
         for line in output.split('\n'):
             if term in line:
                 value = line.replace(term, '').strip()
